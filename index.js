da3aab3a39548dd228ee68677d640d77179365d9
Priyanshu <prynsu@yahoo.com>
2021-04-26 03:31:05 +0530

    API V1

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("epress app running")
+const express = require('express');
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+
+const app = express();
+app.use(bodyParser.json())
+
+app.use(cors())
+
+// DB connect
+const initializeDBConnect = require("./db/db.connect.js")
+initializeDBConnect()
+
+// Middlewares
+const errorHandler404 = require("./middlewares/errorHandler404.middleware.js")
+const errorHandler500 = require("./middlewares/errorHandler500.middleware.js")
+
+// Routers
+const products = require("./routers/products.v1.router.js")
+const users = require("./routers/users.v1.router.js")
+
+app.use("/products", products)
+app.use("/users", users)
+
+
+app.get('/', (req, res) => {
+  res.send("Welcome to Agate's Rest API! \n Building...")
+});
+
+app.use(errorHandler404)
+
+app.use(errorHandler500)
+
+app.listen(8000, () => {
+  console.log('server started on port 8000');
+});
---
+++ routers/products.v1.router.js
@@ -0,0 +1,68 @@
+const express = require("express")
+const router = express.Router()
+const { extend } = require("lodash")
+
+// Model
+const { Product } = require("../models/products.model.js")
+
+router.route("/")
+  .get(async (req, res) => {
+ try {
+   const products = await Product.find({})
+   res.json({success: true, products})
+ } catch(error){
+   res.json({success: false, message: "Product fetch failed", errorMessage: error.message})
+ }
+})
+  .post(async (req, res) => {
+  const saveProd = req.body
+  try{
+    const prodToSave = new Product(saveProd)
+    const prodSaved = await prodToSave.save()
+    res.json({status: true, prodSaved})
+  } catch (err) {
+    res.json({success: false, message: "product not saved", errorMessage: err.message})
+  }
+})
+
+router.param("id", async (req, res, next, id) => {
+  try {
+    const product = await Product.findById(id)
+
+    if (!product) {
+      return res.status(404).json({ success: false, message: "error getting product" })
+    }
+
+    req.product = product
+    next()
+  } catch (error) {
+    res.status(400).json({ success: false, message: "error while retrieving product" })
+  }
+})
+
+router.route("/:id")
+  .get((req, res) => {
+    let {product} = req
+    product.__v = undefined
+    res.json({ success: true, product })
+  })
+  .post(async (req, res) => {
+    const updateProduct = req.body;
+    let { product } = req;
+
+
+    product = extend(product, updateProduct)
+    product = await product.save()
+
+    res.json({ status: true, product })
+  })
+  .delete(async (req, res)=> {
+    let {product} = req
+
+    await product.remove()
+
+    product.deleted = true;
+
+    res.json({success: true, product})
+  })
+module.exports = router
